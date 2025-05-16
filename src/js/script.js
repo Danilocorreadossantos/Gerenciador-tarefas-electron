9e86d4cfc02032eb781149189d63d3ad31e22286
Danilo Correa <danilocorrea.santos@hotmail.com>
2025-05-15 22:22:56 -0300

    Funcionalidade dos botões adicionadas

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,32 @@
 function addTask() {
-    //alert("Funçao de adicionar tarefa")
-    var input = document.getElementById("input-tarefa").value
-    console.log(input)
+    //pega o imput
+    var input = document.getElementById("input-tarefa")
+
+    //Pega o valor do Input
+    var inputvalor = input.value
+
+    console.log(input.value)
+    // Limpar o input
+    input.value = ""
+
+
+    var list = document.getElementById("lista-de-tarefas")
+
+    //criar elemento LI
+    var li = document.createElement("li")
+
+    //adicionar o valor do input para o LI
+    //li.innerText = inputvalor;
+    //li.innerHTML = "Minhas Tarefas<button>Excluir</button>";
+  li.innerHTML = `${inputvalor} <button onclick="this.parentElement.remove()" >🗑️</button>`;
+
+    //adicionar a funçao de marcar a tarefa como concluida
+    li.onclick = function(){
+        li.className = "tarefa-concluida";
+    } 
+
+    //adicionar o LI na lista de tarefas
+    list.appendChild(li)
+
+
 }
